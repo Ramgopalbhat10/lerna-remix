eb62471adede5a0187c062b8fdc575a8cb1763b6
Subbu <ramgopalbhat10@gmail.com>
2022-06-21 20:52:20 +0530

    Initial commit

---
+++ packages/blog/app/components/markdown/CodeHighlight.tsx
@@ -0,0 +1,10 @@
+import { Prism } from "@mantine/prism";
+
+export function CodeHighlight({ code }: { code?: string }) {
+  return (
+    <>
+      <p>Prism code highlight</p>
+      {/* <Prism language="tsx">{code}</Prism> */}
+    </>
+  );
+}
